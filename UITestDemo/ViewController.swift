805cd4050fe5839a9e6bf0c762f3d80ce3cc8117
oCanKhacNguyen <can.khac.nguyen@sun-asterisk.com>
2019-05-02 15:57:58 +0700

    testing

--- UITestDemo/ViewController.swift
+++ UITestDemo/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: false)
+
+
+        
     }
 
     override func viewDidLayoutSubviews() {
